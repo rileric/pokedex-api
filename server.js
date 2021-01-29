9f0bf885b1f95587f628930ae5ccc47fb4e8b9ab
Eric Riley <eriley92@gmail.com>
2021-01-28 21:58:09 -0600

    handle /types

--- server.js
+++ server.js
@@ -2,15 +2,16 @@ const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
-
 app.use(morgan('dev'));
 
-app.use((req, res) => {
-  res.send('Hello, world!');
-});
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`,
+                     `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`];
+
+function handleGetTypes(req, res) {
+    res.json(validTypes);
+}
 
 const PORT = 8000;
-
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
 });
